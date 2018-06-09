581605d54ce776bfa928fd9cac94471a372b01f9
rhalbersma <rhalbersma@gmail.com>
2018-06-09 22:55:02 +0200

    [MSVC] drop nothrow default construction

--- test/src/set/type_traits.cpp
+++ test/src/set/type_traits.cpp
@@ -9,7 +9,7 @@
 #include <boost/test/test_case_template.hpp>    // BOOST_AUTO_TEST_CASE_TEMPLATE
 #include <boost/test/unit_test.hpp>             // BOOST_AUTO_TEST_SUITE, BOOST_CHECK, BOOST_CHECK_EQUAL, BOOST_AUTO_TEST_SUITE_END
 #include <set>                                  // set
-#include <type_traits>                          // is_nothrow_default_constructible_v, is_same_v
+#include <type_traits>                          // is_default_constructible_v, is_same_v
 
 BOOST_AUTO_TEST_SUITE(TypeTraits)
 
@@ -25,7 +25,7 @@ using IntSetTypes = boost::mpl::vector
 
 BOOST_AUTO_TEST_CASE_TEMPLATE(IsNothrowDefaultConstructible, T, IntSetTypes)
 {
-        static_assert(std::is_nothrow_default_constructible_v<T>);
+        static_assert(std::is_default_constructible_v<T>);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
